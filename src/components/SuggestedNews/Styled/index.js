30fbe34087f12284b0171cb18739b81e940df64e
Jón <Lorange>
2019-07-29 11:42:03 +0000

    rammi OUT

--- src/components/SuggestedNews/Styled/index.js
+++ src/components/SuggestedNews/Styled/index.js
@@ -21,7 +21,6 @@ export const NewsContainer = styled.div`
   height: 100%;
   display: grid;
   grid-template-rows: repeat(4, 150px);
-  margin-bottom: 50px;
 `
 export const Title = styled.p`
   font-family: "Open Sans", sans-serif;
